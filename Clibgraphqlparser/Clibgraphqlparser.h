fe64cff40fc36c6f28bdd3d42b4ed99c903d874c
ryanbillard <ryan.billard@shopify.com>
2018-02-04 11:01:08 -0500

    Initial Commit

---
+++ Clibgraphqlparser/Clibgraphqlparser.h
@@ -0,0 +1,19 @@
+//
+//  Clibgraphqlparser.h
+//  Clibgraphqlparser
+//
+//  Created by Ryan Billard on 2018-02-04.
+//  Copyright © 2018 Ryan Billard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Clibgraphqlparser.
+FOUNDATION_EXPORT double ClibgraphqlparserVersionNumber;
+
+//! Project version string for Clibgraphqlparser.
+FOUNDATION_EXPORT const unsigned char ClibgraphqlparserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clibgraphqlparser/PublicHeader.h>
+
+
